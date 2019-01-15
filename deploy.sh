82ab1021b0a2061ba20b0e0f926c18813284e661
wangw <wangw@inke.cn>
2019-01-15 17:56:38 +0800

    Publish Book

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,19 @@
-git checkout gh-pages
+function print() {
+    echo ""
+    echo $1
+    echo ""
+}
+
+print "1. cleanup gh-pages"
+
+git branch -d gh-pages
+git checkout -b gh-pages
 git clean -df
+git merge master
+
+print "2. build and publish"
+
 gitbook build
 cp -r _book/* .
 git commit -a -m "Publish Book"
-git push
+git push -f -u origin master
